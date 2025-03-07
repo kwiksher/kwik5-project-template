cb845825e460a664dec410547b51d73ca563170d
kwiksher <yamamoto@kwiksher.com>
2025-03-07 01:56:34 +0000

    Update kwik exporter kwik/base-proj/Solar2D/plugin/kwik/template

---
+++ UXP/kwik-exporter/kwik/base-proj/Solar2D/plugin/kwik/template/components/pageX/interaction/defaults/shake.lua
@@ -0,0 +1,15 @@
+local M = {
+  name = "shake0",
+  class="shake",
+  properties = {
+    target = NIL,
+    type   = NIL,
+    isActive = true
+  },
+  actions = {
+    onComplete = NIL
+  }
+
+}
+
+return M
