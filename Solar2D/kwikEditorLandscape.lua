48a990148170072d6ccfff905846271af50e7cc6
Naoya Yamamoto <yamamoto@kwiksher.com>
2025-03-27 16:55:00 +0900

    FIX skin

--- Solar2D/kwikEditorLandscape.lua
+++ Solar2D/kwikEditorLandscape.lua
@@ -1,59 +1,14 @@
-------------------------------------------------------------------------------
---
--- This file is part of the Corona game engine.
--- For overview and more information on licensing please refer to README.md
--- Home page: https://github.com/coronalabs/corona
--- Contact: support@coronalabs.com
---
-------------------------------------------------------------------------------
-
--- simulator =
--- {
--- 	device = "ios-phone",
--- 	screenOriginX = 85,
--- 	screenOriginY = 77,
--- 	screenWidth = 1920,
--- 	screenHeight = 1080,
--- 	safeScreenInsetTop = 44 * 3,
--- 	safeScreenInsetLeft = 0 * 3,
--- 	safeScreenInsetBottom = 34 * 3,
--- 	safeScreenInsetRight = 0 * 3,
--- 	safeLandscapeScreenInsetTop = 0 * 3,
--- 	safeLandscapeScreenInsetLeft = 44 * 3,
--- 	safeLandscapeScreenInsetBottom = 21 * 3,
--- 	safeLandscapeScreenInsetRight = 44 * 3,
--- 	iosPointWidth = 375,
--- 	iosPointHeight = 812,
--- 	deviceImage = "iPhoneX.png",
--- 	displayManufacturer = "Apple",
--- 	displayName = "kwik Editor",
--- 	screenDressing = "iPhoneXScreenDressing.png",
--- 	statusBarDefault = "iPhoneXStatusBarBlack.png",
--- 	statusBarTranslucent = "iPhoneXStatusBarWhite.png",
--- 	statusBarBlack = "iPhoneXStatusBarBlack.png",
--- 	statusBarLightTransparent = "iPhoneXStatusBarWhite.png",
--- 	statusBarDarkTransparent = "iPhoneXStatusBarBlack.png",
--- 	windowTitleBarName = "Kwik Editor",
--- 	defaultFontSize = 17 * 3,		-- Converts default font point size to pixels.
--- }
 simulator =
 {
-	device = "ios-tablet",
+	device = "desktop-1920x1080",
 	screenOriginX = 0,
 	screenOriginY = 0,
 	screenWidth = 590,
 	screenHeight = 960,
-	iosPointWidth = 590,
+  iosPointWidth = 590,
 	iosPointHeight = 960,
-	safeLandscapeScreenInsetStatusBar = 20,
-	safeScreenInsetStatusBar = 20,
+	deviceImage = nil,
 	displayManufacturer = "Kwiksher",
-	displayName = "iPad",
-	statusBarDefault = "iPadMiniStatusBarBlack.png", 
-	statusBarTranslucent = "iPadMiniStatusBarWhite.png", 
-	statusBarBlack = "iPadMiniStatusBarBlack.png", 
-	statusBarLightTransparent = "iPadMiniStatusBarWhite.png",
-	statusBarDarkTransparent = "iPadMiniStatusBarBlack.png",
-	windowTitleBarName = "Kwik @1x",
-	defaultFontSize = 17,
+	displayName = "Kwik Landscape",
+	windowTitleBarName = "Kwik Editor Landscape"
 }
