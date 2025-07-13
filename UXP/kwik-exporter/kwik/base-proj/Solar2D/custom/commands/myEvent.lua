16103512379de45c52386226a2981c0afe1f31e0
Naoya Yamamoto <yamamoto@kwiksher.com>
2025-07-13 19:35:54 +0900

    cp -rf Solar2D and Photoshop to UXP/kwik-exporter/kwik/base-proj

--- UXP/kwik-exporter/kwik/base-proj/Solar2D/custom/commands/myEvent.lua
+++
@@ -1,9 +0,0 @@
-local instance = require("plugin.kwik.commands.kwik.baseCommand").new(
-  function (params)
-    local e     = params.event
-    local UI    = e.UI
-    print("commands.myBookEvent")
-  end
-)
---
-return instance
