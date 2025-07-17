58929f92e31471cfedefeb5705c3a6d0eb7ec933
kwiksher <yamamoto@kwiksher.com>
2025-07-17 13:49:46 +0000

    Update kwik exporter kwik/base-proj/Solar2D/lua_modules/kwiksher/resources/template

---
+++ UXP/kwik-exporter/kwik/base-proj/Solar2D/lua_modules/kwiksher/resources/template/components/pageX/replacement/defaults/video.lua
@@ -0,0 +1,18 @@
+local M = {
+  name = "video0",
+  class="video",
+  properties = {
+    target = NIL,
+    autoPlay = true,
+    isLocal = true,
+    keepImage = true,
+    loop = true,
+    rewind = true,
+    url = "",
+ },
+ actions = {
+  actionName = "",
+ }
+}
+
+return M
