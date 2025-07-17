48f9710d1f1fc1dfe05f95cab91c62a7d7ef3f36
kwiksher <yamamoto@kwiksher.com>
2025-07-17 13:49:46 +0000

    Update kwik exporter kwik/base-proj/Solar2D/lua_modules/kwiksher/resources/template

---
+++ UXP/kwik-exporter/kwik/base-proj/Solar2D/lua_modules/kwiksher/resources/template/components/pageX/physics/defaults/properties.lua
@@ -0,0 +1,17 @@
+local M = {
+  name = "properties",
+  class="page",
+  -- actionName = "",
+  properties = {
+      orientation = "landscapeLeft",
+      -- orientation= "portraitUpsideDown"
+      invert = false,
+      scale = 1,
+      gravityX = 0,
+      gravityY = 9.8,
+      drawMode = "Hybrid",
+      walls = {top=false, bottom=true, left=false, right=false}
+  },
+}
+
+return M
